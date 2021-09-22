b05130a3ed67f1b7649bd755c043fb04a05ac472
Robin Das <dassrobin77@gmail.com>
2021-09-22 19:34:22 +0530

    more fixes

--- convert_to_json.py
+++ convert_to_json.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 cols=["pg","word","definition","sentence","category","sample","synonyms"]
-data = pd.read_csv('./ISLT_data.csv', sep=',', engine='python', usecols=cols,na_values = [''])
+data = pd.read_csv('ISLT_data.csv', sep=',', engine='python', usecols=cols,na_values = [''])
 json_data = data.to_json(orient = "records")
 print(json_data)
